f6edd0cf3221cbcf225aa9fc983ddf42eb253ae9
Edwin Wen <edwinwen75@gmail.com>
2025-02-10 20:33:09 -0500

    Feb. 4th meeting

--- cutscenes.gd
+++ cutscenes.gd
@@ -3,17 +3,29 @@ var cutscene_num = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Sprite2D.self_modulate.a = 0
-	$Label.self_modulate.a = 0
-	$UserPrompt.self_modulate.a = 0
-
+	$Sprite2D.self_modulate.a = 1
+	$Label.self_modulate.a = 1
+	$UserPrompt.self_modulate.a = 1
+	$ColorRect.self_modulate.a = 1
+	
+	start_animation()
+	
+func start_animation():
+	$AnimationPlayer.play("fade_out")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("advanceCutscene"):
 		cutscene_num += 1
-		changeCutscene()
+		continue_animation()
 		
+func continue_animation():
+	$AnimationPlayer.play("fade_in")
+	await get_tree().create_timer(2).timeout
+	changeCutscene()
+	$AnimationPlayer.play("fade_out")
+	
+
 func changeCutscene():
 	if cutscene_num == 1:
 		$Sprite2D.texture = ResourceLoader.load("res://Art/scene1.png")
@@ -37,5 +49,10 @@ func changeCutscene():
 	elif cutscene_num == 5:
 		$Sprite2D.texture = ResourceLoader.load("res://Art/scene5.png")
 		$Label.text = "Test journey 505"
+	
+	else:
+		$Sprite2D.self_modulate.a = 0
+		$Label.self_modulate.a = 0
+		$UserPrompt.self_modulate.a = 0
 		
 		
