cc3c84a1e98be3ea6849434038e18ab48028a483
Edwin Wen <edwinwen75@gmail.com>
2025-03-25 16:01:37 -0400

    March 25 meeting

--- main.gd
+++ main.gd
@@ -1,14 +1,28 @@
 extends Node2D
 
+var enemy = preload("res://enemy.tscn")
+
+@export var randomVar = 10
+@export var enemies: Array[PackedScene]
+
+var max_enemies = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	add_enemy()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func add_enemy() -> void:
+	for e in range(max_enemies):
+		var enemyNode = enemy.instantiate()
+		
+		enemyNode.position = $spawnLocations/spawn1.position
+		
+		add_child(enemyNode)
 
 
 func _on_main_menu_start_game() -> void:
