7508ca54eee75d87aae261990e31f0ab4b0003bc
Edwin Wen <edwinwen75@gmail.com>
2025-04-08 15:54:18 -0400

    April 8 meeting

--- enemy.gd
+++ enemy.gd
@@ -9,19 +9,34 @@ enum STATES {IDLE, LOCKIN, ATTACK}
 var state = STATES.IDLE
 var direction = Vector2(-1, 0)
 
+func _process(delta: float) -> void:
+	$AnimatedSprite2D.play("walk")
+	if direction.x >= 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+
 func _physics_process(delta: float) -> void:
-
-	velocity = direction * SPEED * delta * 6
-
-	move_and_slide()
+	if state == STATES.ATTACK:
+		velocity = Vector2.ZERO
+	else:
+		velocity = direction * SPEED * delta * 6
+		move_and_slide()
 
 
 func _on_timer_timeout() -> void:
-	gen_angle()
+	if state == STATES.IDLE:
+		var angle = gen_angle()
+		direction = Vector2(cos(angle), sin(angle)).normalized()
+	elif state == STATES.LOCKIN:
+		follow_player()
+		direction = direction.normalized()
 
-func gen_angle() -> void:
+func follow_player() -> void:
+	direction = get_parent().get_node("Player").position - position
+
+func gen_angle() -> float:
 	var angle
-		
 	
 	if randf() > 0.3:
 		
@@ -31,4 +46,27 @@ func gen_angle() -> void:
 			angle = randf_range(PI/2, 3*PI/2)
 		
 	else:
-		pass
+		
+		if direction.x <= 0:
+			angle = randf_range(-PI/2, PI/2)
+		else:
+			angle = randf_range(PI/2, 3*PI/2)
+			
+	return angle
+
+
+func _on_lock_in_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		state = STATES.LOCKIN
+
+func _on_lock_in_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		state = STATES.IDLE
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		state = STATES.ATTACK
+		
+func _on_attack_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		state = STATES.LOCKIN
